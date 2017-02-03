2a0758d8de15337ca1c9d3e2e6e1ecbcf4066aef
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2017-02-03 22:08:08 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,15 +7,9 @@ ping -c 10 www.vg.no
 if [ ! -f /opt/steamcmd/csgo/finish ]; then
     ./steamcmd.sh +login anonymous +force_install_dir /opt/steamcmd/csgo +app_update 740 validate +quit
     echo "Done" > /opt/steamcmd/csgo/finish
-fi
-cd /opt/steamcmd/csgo
-
-if [ -f "~/.steam/sdk32/steamclient.so" ];
-then
-    echo "~/.steam/sdk32/steamclient.so already exists"
-else
     export LD_LIBRARY_PATH=/opt/steamcmd/csgo:/opt/steamcmd/csgo/bin:{$LD_LIBRARY_PATH}
     mkdir -p ~/.steam/sdk32/
     chmod 750 -R ~/.steam/
     ln -s /opt/steamcmd/linux32/steamclient.so ~/.steam/sdk32/
 fi
+cd /opt/steamcmd/csgo
